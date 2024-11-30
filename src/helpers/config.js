8d2659aae10b00b09b8a14e9575430d6d837c511
anvilm <211anvil112@gmail.com>
2024-12-01 02:12:46 +0300

    fix

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,13 +1,3 @@
 export const APP_NAME = "CSGOMAPS";
 
-export const API_URL = "http://localhost:8000/api/";
-
-export const APP_DOMAIN = "csgomaps.blessc.ru";
-
-export const APP_URL = "https://" + APP_DOMAIN;
-
-export const TG_BOT_URL = "https://t.me/UnboundBlessc_bot";
-
-export const TG_SUPPORT_URL = "https://t.me/UnboundBlesscSupport_bot";
-
-export const EMAIL_SUPPORT = "support@" + APP_DOMAIN;
+export const API_URL = "https://csgomaps.blessc.ru/api/";
